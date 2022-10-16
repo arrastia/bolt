5c52815b241345580cbbeee582a4d950dc357af9
Arrastia <arrastia@pm.me>
2022-10-16 22:17:36 +0200

    ADD - stickers assets

---
+++ src/assets/stickers/index.ts
@@ -0,0 +1,8 @@
+import car from './car.json';
+import coin from './coin.json';
+import money from './money.json';
+import moon from './moon.json';
+import phone from './phone.json';
+import sun from './sun.json';
+
+export { car, coin, money, moon, phone, sun };
