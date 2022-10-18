7c4dda485e15c25a797bff25eb8d7520a6e69df4
Arrastia <arrastia@pm.me>
2022-10-18 20:57:47 +0200

    UPDATE - Layout styles

--- src/ui/components/Layout/Layout.styles.ts
+++ src/ui/components/Layout/Layout.styles.ts
@@ -1,9 +1,12 @@
 import styled from 'styled-components';
 
 const Layout = styled('main')`
+  background: rgb(255 255 255 / 31%);
+  backdrop-filter: saturate(180%) blur(20px);
+  /* box-shadow: 0 10px 70px 1px rgba(0, 0, 0, 0.56); */
+  height: 100%;
   padding-left: env(safe-area-inset-left);
   padding-right: env(safe-area-inset-right);
-  height: 100%;
 `;
 
 export const Styles = { Layout };
--- src/ui/components/Layout/Layout.tsx
+++ src/ui/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
 import { Header } from 'ui/components/Header';
+import { Places } from '../Places';
 import { Styles } from './Layout.styles';
 
 export const Layout = () => {
   return (
     <Styles.Layout>
       <Header />
+      <Places />
       <Outlet />
     </Styles.Layout>
   );
