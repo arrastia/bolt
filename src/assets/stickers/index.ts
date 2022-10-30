09a30faf0c4a6f99bde30e8f734167f33e5dc248
Arrastia <arrastia@pm.me>
2022-10-30 10:01:29 +0100

    ADD - compass & mailbox sticker jsons

--- src/assets/stickers/index.ts
+++ src/assets/stickers/index.ts
@@ -1,8 +1,10 @@
 import car from './car.json';
 import coin from './coin.json';
+import compass from './compass.json';
+import mailbox from './mailbox.json';
 import money from './money.json';
 import moon from './moon.json';
 import phone from './phone.json';
 import sun from './sun.json';
 
-export { car, coin, money, moon, phone, sun };
+export { car, coin, compass, mailbox, money, moon, phone, sun };
