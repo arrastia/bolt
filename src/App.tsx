c16c466228aed0ad789ff0e47d9c24704a3c80cb
Arrastia <arrastia@pm.me>
2022-10-30 10:04:38 +0100

    ADD - lazy loading & routes configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { GlobalStyles } from 'Global.styles';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyles } from './Global.styles';
+
+import { routes } from 'configuration/routes';
 
 import { Layout } from 'ui/components/Layout';
 import { Map } from 'ui/components/Map';
 
-import { NotFound } from 'ui/pages/NotFound';
-import { SignUp } from 'ui/pages/SignUp';
-
 import { light } from 'ui/styles/themes';
+
+const NotFound = lazy(() => import('ui/pages/NotFound'));
+const SignUp = lazy(() => import('ui/pages/SignUp'));
 
 export function App() {
   return (
@@ -18,8 +22,22 @@ export function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route element={<SignUp />} index={true} />
-            <Route element={<NotFound />} path={'*'} />
+            <Route
+              element={
+                <Suspense fallback={<div>LOADING SignUp</div>}>
+                  <SignUp />
+                </Suspense>
+              }
+              index={true}
+            />
+            <Route
+              element={
+                <Suspense fallback={<div>LOADING NotFound</div>}>
+                  <NotFound />
+                </Suspense>
+              }
+              path={routes.NOT_FOUND}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
