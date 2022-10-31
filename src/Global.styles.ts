0ac4d7b18eb6ef04b3c477155ce63736e14b2d8d
Arrastia <arrastia@pm.me>
2022-10-31 18:05:56 +0100

    ADD - app fonts

--- src/Global.styles.ts
+++ src/Global.styles.ts
@@ -1,6 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { EuclidCircularBRegular, EuclidCircularBSemibold } from 'assets/fonts';
+
 export const GlobalStyles = createGlobalStyle`
+    @font-face {
+      font-family: 'EuclidCircularBRegular';
+      font-weight: 400;
+      src: local('EuclidCircularBRegular'), url(${EuclidCircularBRegular}) format('truetype');
+    }
+    
+    @font-face {
+      font-family: 'EuclidCircularBSemibold';
+      font-weight: 700;
+      src: local('EuclidCircularBSemibold'), url(${EuclidCircularBSemibold}) format('truetype');
+    }
+
     * {
       box-sizing: border-box;
       margin: 0;
@@ -8,6 +22,8 @@ export const GlobalStyles = createGlobalStyle`
     }
 
     body {
+      background-color: ${({ theme: { colors } }) => colors.backgroundFallback};
+      color: ${({ theme: { colors } }) => colors.text};
       font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
       sans-serif;
       margin: 0;
---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,4 @@
+import EuclidCircularBRegular from './EuclidCircularB-Regular.woff2';
+import EuclidCircularBSemibold from './EuclidCircularB-Semibold.woff2';
+
+export { EuclidCircularBRegular, EuclidCircularBSemibold };
