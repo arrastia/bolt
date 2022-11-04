423c8f09db9444b420a184e52192b884401a1e2b
Arrastia <arrastia@pm.me>
2022-11-04 23:59:11 +0100

    UPDATE - is dark mode state to use recoil effects to persist data

--- src/ui/stores/UserStore/UserStore.ts
+++ src/ui/stores/UserStore/UserStore.ts
@@ -1,8 +1,8 @@
 import { atom } from 'recoil';
 
-// import { THEME_KEY } from 'configuration/constants';
+import { THEME_KEY } from 'configuration/constants/keys';
 
-// import { getItem, setItem } from 'ui/utils/StorageUtils';
+import { getItem, setItem } from 'storage';
 
 export const tokenState = atom<CryptoKey | string | null>({
   key: 'tokenState',
@@ -11,14 +11,14 @@ export const tokenState = atom<CryptoKey | string | null>({
 
 export const isDarkModeState = atom<boolean>({
   key: 'isDarkModeState',
-  default: false
-  // effects: [
-  //   ({ onSet, setSelf, trigger }) => {
-  //     if (trigger === 'get') setSelf(JSON.parse(getItem<string>(THEME_KEY)));
+  default: false,
+  effects: [
+    ({ onSet, setSelf, trigger }) => {
+      if (trigger === 'get') setSelf(JSON.parse(getItem<string>(THEME_KEY)));
 
-  //     onSet(newValue => setItem(THEME_KEY, newValue));
-  //   }
-  // ]
+      onSet(newValue => setItem(THEME_KEY, newValue));
+    }
+  ]
 });
 
 export const isMenuOpenState = atom<boolean>({
