1001fa2915b850e1ff5d01a3da893a87041ba39d
Arrastia <arrastia@pm.me>
2022-11-07 23:57:44 +0100

    ADD - clock & loading stickers

--- src/assets/stickers/index.ts
+++ src/assets/stickers/index.ts
@@ -1,10 +1,12 @@
 import car from './car.json';
+import clock from './clock.json';
 import coin from './coin.json';
 import compass from './compass.json';
+import loading from './loading.json';
 import mailbox from './mailbox.json';
 import money from './money.json';
 import moon from './moon.json';
 import phone from './phone.json';
 import sun from './sun.json';
 
-export { car, coin, compass, mailbox, money, moon, phone, sun };
+export { car, clock, coin, compass, loading, mailbox, money, moon, phone, sun };
